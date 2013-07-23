8c57e791b5540505c487c4913edba9da151b45a0
mkdir404 <ricardo.alba@hotmail.com>
2013-07-22 21:25:40 -0500

    otro cambio

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
 	<div id='container-page'class='container' style='height:2000px;'>
 
 
-	<p style='font-size:25px;margin-top:100px; margin-left:60px; width:300px;'></p>
-
+	<p style='font-size:25px;margin-top:100px; margin-left:60px; width:300px;'>Vamos a poner otro texto </p>
 	
 	</div> <!--end conteiner page -->
 		
