113f1b00e407dc29b715a01df3515b1df2861a71
mkdir404 <ricardo.alba@hotmail.com>
2013-07-22 21:19:47 -0500

    Cambio en index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<div id='container-page'class='container' style='height:2000px;'>
 
 
-	<p style='font-size:25px;margin-top:100px; margin-left:60px; width:300px;'>Toda la información aquí contenida puede tener errores debido a que es un proyecto aun beta, además de que puede no visualizarse bien en EI8 </p>
+	<p style='font-size:25px;margin-top:100px; margin-left:60px; width:300px;'></p>
 
 	
 	</div> <!--end conteiner page -->
